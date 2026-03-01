8c5bc8e1e31df9d9c8a436ab230fe290e8994bf9
Anitesh Karak <aniteshkarak@gmail.com>
2026-03-01 22:39:02 +0530

    Add mid-insert and search to linked list
    Implement MIdinsert(int val,int pos) to insert a node at a given position (0-based) with validation for negative and out-of-range positions, and add search(int key) to return the index of the first match or -1 if not found. Update main(): reorder initial pushes, call MIdinsert(4,1), comment out some previous test calls, and print the search result. Compiled executable (ll.exe) was also updated.

--- DSA/LInked List/ll.cpp
+++ DSA/LInked List/ll.cpp
@@ -68,6 +68,30 @@ public:
         tail = temp; 
     }
 
+    void MIdinsert(int val,int pos){
+        if (pos<0){
+            cout <<"Invalid Pos\n";
+            return;
+        }
+
+        if (pos==0){
+            push_front(val);
+            return;
+        }
+
+        Node* temp = head;
+        for(int i=0; i<pos-1;i++){
+            if(temp == NULL){
+                cout << "Invalid Position\n";
+                return;
+            }
+            temp = temp-> next;
+        }
+        Node* newNode = new Node(val);
+        newNode -> next = temp -> next;
+        temp->next = newNode;
+    }
+
     void printLL(){ // Print the Linked List
         Node* temp = head;
         while(temp != NULL){
@@ -76,20 +100,37 @@ public:
         }
         cout << "NULL" << endl;
     }
+
+    int search (int key){
+        Node* temp = head;
+        int idx = 0;
+        
+        while(temp != NULL){
+            if(temp ->data == key){
+                return idx;
+            }
+
+            temp = temp -> next;
+            idx++;
+        }
+
+        return -1;
+    }
 };
 
 int main (){
     List ll;
 
-    ll.push_front(1);
-    ll.push_front(2);
     ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
 
-    ll.push_back(4);
-    
-    ll.pop_front();
+    // ll.push_back(4);
+    // ll.pop_front();
+    // ll.pop_back();
 
-    ll.pop_back();
-
+    ll.MIdinsert(4,1);
     ll.printLL();
+
+    cout << "Search key present at this idx = "<< ll.search(3) << endl;
 }
