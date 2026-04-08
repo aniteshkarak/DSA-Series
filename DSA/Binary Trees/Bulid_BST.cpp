219fa676d1fca31d16a6ac0aefc56d26fa9d4d39
Anitesh Karak <aniteshkarak@gmail.com>
2026-04-08 10:09:35 +0530

    Add BST implementations and example programs
    Add two C++ BST example programs and their compiled executables. DSA/Binary Trees/BST.cpp implements insert, buildBST and search with a sample main that tests searching; DSA/Binary Trees/Bulid_BST.cpp (typo in name preserved) implements insert, buildBST and inorder traversal with a sample main that prints the tree. Corresponding .exe binaries were added for each source.

---
+++ DSA/Binary Trees/Bulid_BST.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+
+    if(val < root->data){
+        root->left = insert(root->left, val);
+    }
+    else{
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> arr){
+    Node* root = NULL;
+    
+    for(int i=0; i<arr.size(); i++){
+        root = insert(root, arr[i]);
+    }
+    return root;
+}
+
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main(){
+    vector<int> arr = {3, 2, 1, 5, 6, 4};
+
+    Node* root = buildBST(arr);
+    inorder(root);
+    cout << endl;
+}
+
