6616a4d2f2940d70c3868c1109bed5d27a79272f
Anitesh Karak <aniteshkarak@gmail.com>
2026-07-14 23:01:42 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.62%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    TreeNode* prev = NULL;
+
+    int minDiffInBST(TreeNode* root) {
+        if(root == NULL){
+            return INT_MAX;
+        }
+
+        int ans = INT_MAX;
+        if(root->left != NULL){
+            int leftMin = minDiffInBST(root->left);
+            ans = min(ans, leftMin);
+        }
+
+        if(prev != NULL){
+            ans = min(ans, root->val - prev->val);
+        }
+        prev = root;
+
+        if(root->right != NULL){
+            int rightMin = minDiffInBST(root->right);
+            ans = min(ans, rightMin);
+        }
+        return ans;
+    }
+};
